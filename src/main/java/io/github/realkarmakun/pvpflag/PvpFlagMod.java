9f09d058a73a3b6da445510193b3868cdb93280d
realkarmakun <20980281+realkarmakun@users.noreply.github.com>
2024-01-20 03:24:14 +0600

    * Add alternative flag logic * Add peacefulIndomitability gamerule for changing logic * Update to 1.20.4 Now whenever peacefulIndomitability is enabled PvP Flag mod would change it's behaviour, and will only allow players with skulls to engage in PvP

--- src/main/java/io/github/realkarmakun/pvpflag/PvpFlagMod.java
+++ src/main/java/io/github/realkarmakun/pvpflag/PvpFlagMod.java
@@ -4,12 +4,17 @@ import io.github.realkarmakun.pvpflag.events.OnPlayerHurtEvent;
 import io.github.realkarmakun.pvpflag.events.PlayerHurtEventCallback;
 import io.github.realkarmakun.pvpflag.networking.PvpFlagNetworkHandler;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.world.level.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PvpFlagMod implements ModInitializer {
     public static final String MOD_ID = "pvpflag";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final GameRules.Key<GameRules.BooleanValue> PEACEFUL_INDOMITABILITY =
+            GameRuleRegistry.register("peacefulIndomitability", GameRules.Category.PLAYER, GameRuleFactory.createBooleanRule(false));
 
     @Override
     public void onInitialize() {
